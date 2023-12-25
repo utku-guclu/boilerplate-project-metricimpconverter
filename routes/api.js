286f56cfa9cf86c9ae5f43c8051d0061984ac6b9
Utku <34550887+utku-guclu@users.noreply.github.com>
2023-12-25 22:14:45 +0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,38 @@
 'use strict';
-const ConvertHandler = require('../controllers/convertHandler.js');
 
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
+const cors = require('cors');
 
 module.exports = function (app) {
 
-  const convertHandler = new ConvertHandler();
-  app.get('/api/convert', (req, res) => {
-    let error
-    const input = req.query.input
-    const initNum = convertHandler.getNum(input)
-    const initUnit = convertHandler.getUnit(input)
-    if (initNum === 'invalid number') {error = 'invalid number'}
-    if (initUnit === 'invalid unit') {if (!error){ error = 'invalid unit'} else {error = 'invalid number and unit'}}
-    if (error){res.json(error)}
-    const returnNum = parseFloat(convertHandler.convert(initNum, initUnit).toFixed(5))
-    const returnUnit = convertHandler.getReturnUnit(initUnit)
-    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
-    res.json({ initNum, initUnit, returnNum, returnUnit, string })
-  })
+  let convertHandler = new ConvertHandler();
+
+  app.use(cors({
+      origin: '*'
+  }));
+
+  app.route('/api/convert').get((req, res) => {
+    let result;
+    const initNum = convertHandler.getNum(req.query.input);
+    const initUnit = convertHandler.getUnit(req.query.input);
+    if (initNum === 'invalid number' && initUnit === 'invalid unit'){
+      result = 'invalid number and unit';
+    } else if (initUnit === 'invalid unit'){
+      result = 'invalid unit';
+    } else if (initNum === 'invalid number'){
+      result = 'invalid number';
+    } else {
+      const returnNum = convertHandler.convert(initNum, initUnit);
+      const returnUnit = convertHandler.getReturnUnit(initUnit);
+      result = {
+        "initNum": initNum,
+        "initUnit": initUnit,
+        "returnNum": returnNum,
+        "returnUnit": returnUnit,
+       "string": convertHandler.getString(initNum, convertHandler.spellOutUnit(initUnit), returnNum, convertHandler.spellOutUnit(returnUnit))
+      };
+    }
+    res.send(result);
+  });
 };
