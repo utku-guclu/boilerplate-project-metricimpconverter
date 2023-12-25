0d601362d656a38d9801723953a148366625d6ab
Utku <34550887+utku-guclu@users.noreply.github.com>
2023-12-25 21:59:17 +0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,50 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    test('Convert a valid input such as 10L: GET request to /api/convert.', () => {
+        chai
+            .request(server)
+            .get('/api/convert')
+            .query({ input: '10L' })
+            .end((err, res) => {
+                assert.equal(res.body.initNum, 10)
+                assert.equal(res.body.initUnit, 'L')
+            })
+    })
+    test('Convert an invalid input such as 32g: GET request to /api/convert.', () => {
+        chai
+            .request(server)
+            .get('/api/convert')
+            .query({ input: '32g' })
+            .end((err, res) => {
+                assert.equal(res.body, 'invalid unit')
+            })
+    })
+    test('Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert', () => {
+        chai
+            .request(server)
+            .get('/api/convert')
+            .query({ input: '3/7.2/4kg' })
+            .end((err, res) => {
+                assert.equal(res.body, 'invalid number')
+            })
+    })
+    test('Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert.', () => {
+        chai
+            .request(server)
+            .get('/api/convert')
+            .query({ input: '3/7.2/4kilomegagram' })
+            .end((err, res) => {
+                assert.equal(res.body, 'invalid number and unit')
+            })
+    })
+    test('Convert with no number such as kg: GET request to /api/convert.', () => {
+        chai
+            .request(server)
+            .get('/api/convert')
+            .query({ input: 'kg' })
+            .end((err, res) => {
+                assert.equal(res.body.initNum, 1)
+            })
+    })
 });
