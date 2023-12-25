837fd9c05c5d443d4094cfc11a8d40e2392d2e58
Utku <34550887+utku-guclu@users.noreply.github.com>
2023-12-25 21:58:16 +0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,21 @@
 'use strict';
-
-const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
-module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
 
+module.exports = function (app) {
+
+  const convertHandler = new ConvertHandler();
+  app.get('/api/convert', (req, res) => {
+    let error
+    const input = req.query.input
+    const initNum = convertHandler.getNum(input)
+    const initUnit = convertHandler.getUnit(input)
+    if (initNum === 'invalid number') {error = 'invalid number'}
+    if (initUnit === 'invalid unit') {if (!error){ error = 'invalid unit'} else {error = 'invalid number and unit'}}
+    if (error){res.json(error)}
+    const returnNum = parseFloat(convertHandler.convert(initNum, initUnit).toFixed(5))
+    const returnUnit = convertHandler.getReturnUnit(initUnit)
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
+    res.json({ initNum, initUnit, returnNum, returnUnit, string })
+  })
 };
